0833414a00fc862b2a5f83f1a76d789ea9d4c463
Felipe Carvalho <feliped3carvalho@gmail.com>
2020-06-04 20:24:08 -0300

    criados base de rotas

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,15 +1,11 @@
-import express from 'express'
+import express from 'express';
+import routes from './routes';
 
 const app = express();
-app.get('/users', (req, res)=> {
-    res.json([
-        'Felipe',
-        'Jurandi',
-        'Dr. Abobinha'
-    ])
-});
 
 
+app.use(express.json());
+app.use(routes);
 
 
 app.listen(3333);
